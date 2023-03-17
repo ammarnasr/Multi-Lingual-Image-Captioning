cd72b4b007a5e2321263272791871f157eafe179
Ammar Nasr <ammarnasraza@gmail.com>
2023-03-17 18:45:15 +0000

    json

--- train_gpt.py
+++ train_gpt.py
@@ -1,35 +1,52 @@
 import os
 import sys
+import json
 import torch
 import pickle
 import argparse
 from tqdm import tqdm
-from args import DemoArgs
 from models import ClipCaptionPrefix
 from torch.nn import functional as nnf
 from torch.utils.data import  DataLoader
 from dataset import ClipGPTFlickr8kDataset
 from transformers import  AdamW, get_linear_schedule_with_warmup
 
-def load_model(model_path):
+def load_model(model_path, args):
     '''
     Load the model from the checkpoint
     :param model_path: path to the model checkpoint
     :return: the model, the arguments and the epoch number
     '''
     epoch_number = int(model_path.split('-')[-1].split('.')[0]) + 1
-    args_path = model_path.replace('.pt', '_args.pkl')
-    with open(args_path, 'rb') as f:
-        args = pickle.load(f)
+
+    #load args from json file
+    args_path = model_path.replace('.pt', '_args.json')
+    
+    # check if the args_path exists
+    if os.path.exists(args_path):
+        with open(args_path, 'r') as f:
+            args_json = json.load(f)
+        args.__dict__.update(args_json)
+    else:
+        # load args from pickle file
+        args_path = model_path.replace('.pt', '_args.pkl')
+        with open(args_path, 'rb') as f:
+            args = pickle.load(f)
+
     model = ClipCaptionPrefix(args.prefix_length, args.lang , clip_length=args.prefix_length_clip, prefix_size=512, num_layers=args.num_layers, mapping_type=args.mapping_type)
     model.load_state_dict(torch.load(model_path, map_location='cpu'))
     return model , args, epoch_number
 
 
-def train(dataset, model, args, lr = 2e-5, warmup_steps = 5000, output_dir = ".", output_prefix: str = "", start_epoch = 0):
+def train(dataset, model, args , start_epoch = 0):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    batch_size = args.bs
+    batch_size = args.batch_size
     epochs = args.epochs
+    lr = args.lr
+    warmup_steps = args.warmup_steps
+    output_dir = args.output_dir
+    model_name = args.model_name
+
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     model = model.to(device)
@@ -41,7 +58,7 @@ def train(dataset, model, args, lr = 2e-5, warmup_steps = 5000, output_dir = "."
     for epoch in range(start_epoch, epochs+start_epoch):
         print(f">>> Training epoch {epoch} out of {epochs+start_epoch}")
         sys.stdout.flush()
-        progress = tqdm(total=len(train_dataloader), desc=output_prefix)
+        progress = tqdm(total=len(train_dataloader), desc=model_name)
         for idx, (tokens, mask, prefix) in enumerate(train_dataloader):
             model.zero_grad()
             tokens, mask, prefix = tokens.to(device), mask.to(device), prefix.to(device, dtype=torch.float32)
@@ -54,16 +71,15 @@ def train(dataset, model, args, lr = 2e-5, warmup_steps = 5000, output_dir = "."
             optimizer.zero_grad()
             progress.set_postfix({"loss": loss.item()})
             progress.update()
-            if (idx + 1) % 10000 == 0:
-                torch.save(model.state_dict(),os.path.join(output_dir, f"{output_prefix}_latest.pt"),)
+            if (idx + 1) % 100 == 0:
+                torch.save(model.state_dict(),os.path.join(output_dir, f"{model_name}_latest.pt"),)
         progress.close()
         if epoch % args.save_every == 0 or epoch == epochs - 1 + start_epoch:
-            model_path = os.path.join(output_dir, f"{output_prefix}-{epoch:03d}.pt")
-            args_path = model_path.replace('.pt', '_args.pkl')
+            model_path = os.path.join(output_dir, f"{model_name}-{epoch:03d}.pt")
+            args_path = model_path.replace('.pt', '_args.json')
             torch.save(model.state_dict(), model_path)
             with open(args_path, 'wb') as f:
-                pickle.dump(args, f)
-
+                json.dump(args.__dict__, f)
     return model
 
 
@@ -71,20 +87,16 @@ def train(dataset, model, args, lr = 2e-5, warmup_steps = 5000, output_dir = "."
 
 
 
-def main(model_path = None):
-        
-
+def main(args):
+    model_path = args.checkpoint
     if model_path is None:
-        args = DemoArgs()
         start_epoch = 0
-        prefix_dim = 640 if args.is_rn else 512
-        model = ClipCaptionPrefix(args.prefix_length, lang=args.lang , clip_length=args.prefix_length_clip, prefix_size=prefix_dim, num_layers=args.num_layers)
+        model = ClipCaptionPrefix(args.prefix_length, lang=args.lang, clip_length=args.clip_length, prefix_size=args.prefix_size, num_layers=args.num_layers)
     else:
         model, args, start_epoch = load_model(model_path)
-        prefix_dim = 640 if args.is_rn else 512
     
     dataset = ClipGPTFlickr8kDataset(args.data, args.prefix_length,lang= args.lang, normalize_prefix=args.normalize_prefix)
-    train(dataset, model, args, output_dir=args.out_dir, output_prefix=args.output_prefix, start_epoch = start_epoch)
+    train(dataset, model, args, start_epoch = start_epoch)
 
 
 if __name__ == '__main__':
@@ -106,6 +118,9 @@ if __name__ == '__main__':
     parser.add_argument('--num_layers',       type=int, default=1, help='Number of layers in the Transformer Mapper')
     parser.add_argument('--mapping_type',     type=str, default='linear', help='The type of mapping between the CLIP features and the prefix features')
     parser.add_argument('--checkpoint',       type=str, default=None, help='The path to the checkpoint to load the model from')
+    parser.add_argument('--lr',               type=float, default=0.01, help='The learning rate')
+    parser.add_argument('--warmup_steps',     type=int, default=1000, help='The number of warmup steps')
+
      
     
     # parse the arguments
@@ -115,12 +130,9 @@ if __name__ == '__main__':
     print('The arguments are:')
     for arg in vars(args):
         print(arg,':\t',  getattr(args, arg))
-    
-    #check if checkpoint is given as argument
-    if args.checkpoint is not None:
-        print("Loading model from checkpoint")
-        model_path = args.checkpoint
-        main(model_path)
-    else:
-        print('Training from scratch')
-        main()
+
+    # save the arguments to a pickle file
+    with open("test_args.pkl", 'wb') as f:
+        pickle.dump(args, f)
+    # return
+    # main(args)
